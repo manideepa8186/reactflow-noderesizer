893992ff6cf9b99246eea5dbd998bb12802f63e5
Manideep <manideep.a@comakeit.com>
2023-06-28 14:40:59 +0530

    reactflow-node resizer bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactFlow, { MiniMap, Background, BackgroundVariant, Controls } from 'reactflow';
 
-function App() {
+import 'reactflow/dist/style.css';
+
+import ResizeableNode
+ from './ResizeableNode';
+const nodeTypes = {
+  ResizeableNode
+};
+
+const initialNodes = { '1':{
+                              id: '1',
+                              type: 'ResizeableNode',
+                              data: { label: 'NodeResizer' },
+                              position: { x: 0, y: 50 },
+                              style: { background: '#fff', border: '1px solid black', borderRadius: 15, fontSize: 12 },
+                            },
+                        '2':{
+                          id: '1',
+                          type: 'ResizeableNode',
+                          data: { label: 'NodeResizer' },
+                          position: { x: 0, y: 50 },
+                          style: { background: '#fff', border: '1px solid black', borderRadius: 15, fontSize: 12 },
+                        }
+                      };
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    className="reactflow-wrapper"
+    style={{ width: "100%", height: "90vh" }}
+  >
+    <ReactFlow
+      nodes={Object.values(initialNodes)}
+      className="react-flow-node-resizer-example"
+      style={{ width: "100%", height: "90vh" }}
+      fitView
+      nodeTypes={nodeTypes}
+    >
+      <Background variant={BackgroundVariant.Dots} />
+      {/* <MiniMap /> */}
+      {/* <Controls /> */}
+    </ReactFlow>
     </div>
   );
 }
-
-export default App;
